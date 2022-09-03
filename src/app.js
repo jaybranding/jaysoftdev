488d2bbde6fa8b97bfb21f46150e0b5567a0681b
jaybranding <j@jaybranding.com>
2022-09-03 20:05:59 +0700

    modified:   public/playground/data/menu-ogw.json 	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (request, response) {
-  response.sendFile("/public/index.html");
+  response.sendFile("/index.html");
 });
