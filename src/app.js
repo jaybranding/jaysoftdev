07f477a3bfc734097f8dee00eba4f33c2341b5ce
jaybranding <j@jaybranding.com>
2022-09-06 10:13:37 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ import express from "express";
 const app = express();
 
 app.get("/", function (request, response) {
-  response.sendFile("/index.html");
+  response.sendFile("../public/index.html");
 });
